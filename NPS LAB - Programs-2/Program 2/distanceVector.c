bb844ad14ebed79d9f58c217534537da546cbf3b
pydivenkat <98642982+pydivenkat@users.noreply.github.com>
2023-03-21 22:32:33 +0530

    Update distanceVector.c

--- NPS LAB - Programs-2/Program 2/distanceVector.c
+++ NPS LAB - Programs-2/Program 2/distanceVector.c
@@ -1,81 +1,60 @@
 #include<stdio.h>
-#include<stdlib.h>
-#define INF 999
-int G[100][100], n, selected[100], P[100], d[100];
-int getMinDistAndNotSelectedNode()
-{
-	int i, min = INF, node;
-	for(i=0;i<n;i++)
-	{
-		if(!selected[i] && min>d[i])
-		{
-			min = d[i];
-			node = i;
+int A[10][10], n, d[10], p[10];
+voidBellmanFord(int s){
+	inti,u,v;
+	for(i=1;i<n;i++){
+		for(u=0;u<n;u++){
+			for(v=0;v<n;v++){
+				if(d[v] > d[u] + A[u][v]){
+					d[v] = d[u] + A[u][v];
+					p[v] = u;
+				}
+			}
 		}
 	}
-	return node;
-}
-void display(int source)
-{
-	printf("For Source %d:\n", source);
-	int i, j;
-	for(i=0;i<n;i++)
-	{
-	j = i;
-	if(j!=source){
-	while(j!=source)
-	{
-		printf("%d->", j);
-		j = P[j];
-	}
-	printf("%d\tdist : %d\n", j, d[i]);
-	}
-	}
-}
-void distanceVectorCalc(int source)
-{
-	int i, minNode, j, count =0;
-	for(i=0;i<n;i++)
-	{
-		d[i] = INF;
-		P[i] = -1;
-	}
-	d[source] = 0;
-	for(j=1;j<=n-1;j++){
-	minNode = getMinDistAndNotSelectedNode();
-	selected[minNode] = 1;
-	for(i=0;i<n;i++)
-	{
-		if(!selected[i] && d[i]>d[minNode]+G[minNode][i])
-		{
-			d[i] = d[minNode] + G[minNode][i];
-			P[i] = minNode;
-			count++;
+	for(u=0;u<n;u++){
+		for(v=0;v<n;v++){
+			if(d[v] > d[u] + A[u][v]){
+				printf("Negative Edge");
+			}
 		}
-	}}
-	for(i=0;i<n;i++)
-	for(j=0;j<n;j++)
-	if(d[j]>d[i]+G[i][j])
-	{
-		printf("Negative edge");
-		//return 0;
 	}
-		display(source);
 }
-int main()
-{
-	int i, j;
-	printf("Enter the no. of routers : ");
-	scanf("%d", &n);
-	printf("Enter the adjacency matrix : ");
+
+int main(){
+	printf("Enter the no. of vertices : ");
+	scanf("%d",&n);
+	printf("Enter the adjacency matrix\n");
+	inti,j;
 	for(i=0;i<n;i++)
-	for(j=0;j<n;j++)
-	scanf("%d", &G[i][j]);
-	i =0;
-	for(i=0;i<n;i++)
-	{
-		distanceVectorCalc(i);
-		for(j=0;j<n;j++){
-		selected[j] = 0;}
+		for(j=0;j<n;j++)
+			scanf("%d",&A[i][j]);
+		
+	int source;
+	for(source=0;source<n;source++){
+		
+		
+		for(i=0;i<n;i++){
+			d[i] = 999;
+			p[i] = -1;
+		}
+		d[source] = 0;
+		
+		BellmanFord(source);
+		
+		printf("Router %d\n",source);
+		
+		for(i=0;i<n;i++){
+			if(i != source){
+				j = i;
+				while(p[j] != -1){
+					printf("%d <- ",j);
+					j = p[j];
+				}
+			}
+			printf("%d\tCost %d\n\n",source,d[i]);
+		}
 	}
+	
+	return 0;
 }
