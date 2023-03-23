1c69091e5bf70182bcdd6410a883f8aa7f51eb06
pydivenkat <98642982+pydivenkat@users.noreply.github.com>
2023-03-24 03:17:54 +0530

    Update client.c

--- NPS LAB - Programs-2/Program 5/client.c
+++ NPS LAB - Programs-2/Program 5/client.c
@@ -1,39 +1,47 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
-void str_echo(FILE *fp,int sockfd)
-{
-	int n=1024,cont;
-	char *buf=malloc(n);
-	while(fgets(buf,n,fp)!=NULL)
-	{
-		//printf("\nEnter the string:\n");
-		//scanf("%s",buf);
-		send(sockfd,buf,n,0);
-	
-	//	printf("%s",buf);
-		while((n=recv(sockfd,buf,n,0))<0);
-			fputs(buf,stdout);
-	}
-	printf("\nEOF\n");
+#include<sys/types.h> 
+#include<sys/socket.h> 
+#include<sys/stat.h> 
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<fcntl.h> 
+#include<arpa/inet.h> 
+#include<netinet/in.h> 
+
+
+void str_cli(FILE *fp,int sockfd)
+{ 
+    int bufs=1024,cont; 
+    char *buff = malloc(bufs); 
+    while((fgets(buff,bufs,fp)!=NULL))
+    {
+        send(sockfd,buff,bufs,0); 
+        if((cont = recv(sockfd,buff,bufs,0)) >=0)
+        { 
+            printf("Server replied :"); 
+            fputs(buff,stdout); 
+        }
+    }
+    printf("str cli\n"); 
+    printf("\nEOF\n"); 
 }
-int main(int argc,char *argv[])
-{
-	int sockfd;
-	struct sockaddr_in servaddr;
-	printf("%s\n",argv[1]);
-	if(sockfd=socket(AF_INET,SOCK_STREAM,0))
-		printf("\nSocket created\n");
-	servaddr.sin_family=AF_INET;
-	servaddr.sin_port=htons(15001);
-	inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
-	
-	connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-	
-	str_echo(stdin,sockfd);
-	close(sockfd);
-}
+
+
+int main()
+{ 
+    int create_socket,ret; 
+    struct sockaddr_in address; 
+    if((create_socket = socket(AF_INET,SOCK_STREAM,0)) >0) 
+        printf("Socket created\n"); 
+    else 
+        printf("Socket creation error"); 
+    address.sin_family = AF_INET; 
+    address.sin_port = htons(15002); 
+    address.sin_addr.s_addr=inet_addr("127.0.0.1");
+    if( (ret=connect(create_socket,(struct sockaddr *) &address,sizeof(address))) ==  0) 
+        printf("Connected\n"); 
+    else 
+        printf("Error in connect"); 
+    str_cli(stdin,create_socket); 
+    return 0; 
+} 
